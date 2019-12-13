bc74c912b2689b4a8a8e03e3baa9deb88c8ae64a
Josue Reyna <j.reyna@hlcn.mx>
2019-12-13 13:04:29 -0600

    Agrega ejemplo 8 Bucle for

---
+++ Py/Ejemplo8.py
@@ -0,0 +1,7 @@
+#Realzar un programa que imprima en pantalla los numeros del 20 al 30
+for x in range(20,31):
+    print(x)
+'''La funcion range puede tener dos parametros, el primero indica el valor
+ inicial que tomara la variable x, cada vuelta del for la variable x toma
+ el valor siguiente hasta llegar al valor indicado por el segundo parametro
+ de la funcion range menos uno. '''
