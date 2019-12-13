cbdd94b6f0cd992262b671098b2ec245ae8124b8
Josue Reyna <j.reyna@hlcn.mx>
2019-12-13 11:28:18 -0600

    Se termino el ejemplo 6

---
+++ Py/Ejemplo6.py
@@ -0,0 +1,12 @@
+#Ejemplo While
+x=1
+suma=0
+while x<=10:
+    valor=int(input("ingrese un valor: "))
+    suma=suma+valor
+    x=x+1
+promedio=suma/10
+print("La suma de los 10 valores es: ")
+print(suma)
+print("El promedio es: ")
+print(promedio)
